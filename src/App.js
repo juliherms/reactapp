a66632b6fd9e8b6248e37c619b3b33d8aa602dfe
Vasconcelos <j.a.vasconcelos@accenture.com>
2019-12-22 11:20:42 -0300

    create a counter in textarea component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component{
   render() {
     return (
     <div>
-      <TextCounter/>
+      <TextCounter limit={10} />
     </div>
     );
   }
--- src/TextCounter.js
+++ src/TextCounter.js
@@ -3,13 +3,27 @@ import React, {Component} from 'react';
 //component responsible to count character in text box
 class TextCounter extends Component{
 
+    static defaultProps = {
+        limit: 15
+    }
+
+    constructor(props){
+        super(props);
+
+        
+        this.state = {
+            totalChars: 0
+        }
+    }
+
     render(){   
+        const { state, props } = this; //extract access
         return (  
             <div>
                 <h1>Test Counter</h1>
                 <textarea />
                 <div>
-                    <strong>Total:</strong>
+                    <strong>Total:</strong> { state.totalChars }/{ props.limit}
                 </div>
             </div>
         )
